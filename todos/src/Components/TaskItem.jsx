f6ceb301fccb9c1ddc01d85e2b76448b5f276b3b
Your Name <you@example.com>
2021-12-25 19:54:01 +0500

    create TaskIte,\m.jsx

---
+++ todos/src/Components/TaskItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import DeleteBtn from "./DeleteBtn";
+
+const TaskItem = ({category, onDoubleClick, task, deleteTask}) => {
+    return (
+        <div
+        className={`taskAndBtn ${category && "complited"} `}
+        onDoubleClick={onDoubleClick}
+      >
+        {task}
+        <DeleteBtn deleteTask={deleteTask} />
+      </div>
+    )
+}
+
+export default TaskItem
