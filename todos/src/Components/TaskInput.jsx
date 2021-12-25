0c2bd2b107fe58f39e551c107b1e5b01849cf2dd
Your Name <you@example.com>
2021-12-25 14:45:33 +0500

    used useState for TaskInput to change value of Input

--- todos/src/Components/TaskInput.jsx
+++ todos/src/Components/TaskInput.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 const TaskInput = (props) => {
+  const [value, setValue] = useState("");
+  const handleOnChange = (e) => {
+    setValue(e.target.value);
+  };
+  const handleOnKeyPress = (e) => {
+    if (e.key == "Enter") {
+      console.log(e.key);
+    }
+  };
   return (
     <div className="inputTask">
       <FontAwesomeIcon icon={faCheck} className="icon" />
-      <Input />
+      <Input
+        value={value}
+        onChange={handleOnChange}
+        onKeyPress={handleOnKeyPress}
+      />
     </div>
   );
 };
@@ -14,5 +27,12 @@ const TaskInput = (props) => {
 export default TaskInput;
 
 const Input = (props) => {
-  return <input placeholder="What needs to be done?" />
-}
+  return (
+    <input
+      placeholder="What needs to be done?"
+      value={props.value}
+      onChange={props.onChange}
+      onKeyPress={props.onKeyPress}
+    />
+  );
+};
